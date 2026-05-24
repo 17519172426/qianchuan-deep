c17a7dd2b2a123dd73637b5fd03f8938eed83259
Developer <dev@example.com>
2026-05-24 23:01:17 +0800

    feat: add Go gRPC client for Python strategy service

---
+++ server/grpc/client.go
@@ -0,0 +1,55 @@
+package grpc
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/example/qianchuan-saas/grpc/strategy"
+)
+
+type Client struct {
+	conn *grpc.ClientConn
+	Stub pb.StrategyServiceClient
+}
+
+func NewClient(addr string) (*Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(ctx, addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{conn: conn, Stub: pb.NewStrategyServiceClient(conn)}, nil
+}
+
+func (c *Client) Close() {
+	if c.conn != nil {
+		c.conn.Close()
+	}
+}
+
+func (c *Client) EvaluateRules(ctx context.Context, rules []*pb.RuleDef, ads []*pb.AdContext) ([]*pb.RuleAction, error) {
+	resp, err := c.Stub.EvaluateRules(ctx, &pb.EvaluateRequest{Rules: rules, Ads: ads})
+	if err != nil {
+		log.Printf("gRPC EvaluateRules failed: %v", err)
+		return nil, err
+	}
+	return resp.Actions, nil
+}
+
+func (c *Client) TestRule(ctx context.Context, rule *pb.RuleDef, ads []*pb.AdContext) (*pb.TestRuleResponse, error) {
+	resp, err := c.Stub.TestRule(ctx, &pb.TestRuleRequest{Rule: rule, Ads: ads})
+	if err != nil {
+		log.Printf("gRPC TestRule failed: %v", err)
+		return nil, err
+	}
+	return resp, nil
+}
