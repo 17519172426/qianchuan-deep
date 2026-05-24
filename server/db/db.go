f515bb6c701fd882e0a11fa295b586fc74bc0eea
Developer <dev@example.com>
2026-05-24 20:01:07 +0800

    feat: add database connection with GORM

---
+++ server/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func Connect(databaseURL string) {
+	var err error
+	DB, err = gorm.Open(postgres.Open(databaseURL), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+}
+
+func AutoMigrate(models ...interface{}) {
+	if err := DB.AutoMigrate(models...); err != nil {
+		log.Fatalf("failed to migrate: %v", err)
+	}
+}
--- server/main.go
+++ server/main.go
@@ -6,10 +6,15 @@ import (
 	"net/http"
 
 	"github.com/example/qianchuan-saas/config"
+	"github.com/example/qianchuan-saas/db"
 )
 
 func main() {
 	cfg := config.Load()
+
+	db.Connect(cfg.DatabaseURL)
+	log.Println("database connected")
+
 	log.Printf("千川投流助手 starting on :%s", cfg.ServerPort)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", cfg.ServerPort), nil))
 }
